3f32716c53b21ee962a349f6f54cecefff10fd93
PakinKantawong <pakinkantawong2749@gmail.com>
2026-03-16 16:38:03 +0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  AUTH_URL: window.location.origin,
-  TASK_URL: window.location.origin,
-  USER_URL: window.location.origin
+  AUTH_URL: https://auth-service-okyi-production.up.railway.app/api/auth/health,
+  TASK_URL: https://task-service-production-32fe.up.railway.app/api/tasks/health,
+  USER_URL: https://user-service-production-7c12.up.railway.app/api/users/health
 };
