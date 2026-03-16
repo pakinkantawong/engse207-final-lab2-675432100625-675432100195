d3ecc11e24f7b67f9ed5366ab0d5b3abfc9de488
PakinKantawong <pakinkantawong2749@gmail.com>
2026-03-16 19:41:32 +0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  AUTH_URL: 'http://localhost:3000',
-  TASK_URL: 'http://localhost:3000',
-  USER_URL: 'http://localhost:3000'
+  AUTH_URL: 'https://auth-service-production-5d84.up.railway.app',
+  TASK_URL: 'https://task-service-production-ad9b.up.railway.app',
+  USER_URL: 'https://user-service-production-e727.up.railway.app'
 };
